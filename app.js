d9c956c7047bdc5587f51a2927b9b26ce04ee902
mlipyan <mlipyan@gmail.com>
2025-01-11 19:48:41 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const port = process.env.PORT || 8000;
+var socketIO = require('socket.io');
+
+const {writeContent, uploadData} = require('./write_data.js');
+
+//const { Client } = require('tplink-smarthome-api');
+//const client = new Client();
+//client.startDiscovery().on('device-new', (device) => {
+// device.getSysInfo().then(console.log);
+//  device.setPowerState(true);
+//});
+
+server = http.createServer(function (req, res) {
+  console.log(`${req.method} ${req.url}`);
+
+
+  // parse URL
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `.${parsedUrl.pathname}`;
+  // based on the URL path, extract the file extention. e.g. .js, .doc, ...
+  const ext = path.parse(pathname).ext;
+  // maps file extention to MIME typere
+  const map = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword'
+  };
+
+  fs.exists(pathname, function (exist) {
+    if(!exist) {
+      // if the file is not found, return 404
+      res.statusCode = 404;
+      res.end(`File ${pathname} not found!`);
+      return;
+    }
+
+    // if is a directory search for index file matching the extention
+    if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+    console.log(pathname);
+    // read file from file system
+    fs.readFile(pathname, function(err, data){
+      if(err){
+        res.statusCode = 500;
+        res.end(`Error getting the file: ${err}.`);
+      } else {
+        // if the file is found, set Content-type and send data
+        res.setHeader('Content-type', map[ext] || 'text/plain' );
+        res.end(data);
+      }
+    });
+  });
+});
+
+var io = socketIO(server);
+// Add the WebSocket handlers
+io.on('connection', function(socket) {
+  socket.on('submit', data => {socket.emit('recieved', writeContent(data))});
+  socket.on('upload', data => {uploadData()});
+});
+
+server.listen(parseInt(port));
+console.log(`Server listening on port ${port}`);
